8e3291f9fcb8b7d491fc7b87a917098bca3dc183
statuc30721 <bubalator@yahoo.com>
2024-12-06 15:33:52 +0100

    Modified security group for syslog server and updated scaling groups.

--- 10-AutoScalingGroup.tf
+++ 10-AutoScalingGroup.tf
@@ -159,3 +159,413 @@ resource "aws_autoscaling_attachment" "ASG01_NY_attachment" {
   alb_target_group_arn   = aws_lb_target_group.ASG01_NY_TG01.arn
 }
 
+#-------------------------------------------------------------------#
+# London Region
+resource "aws_autoscaling_group" "ASG01_LON" {
+  name_prefix           = "ASG01-London-auto-scaling-group-"
+  min_size              = 1
+  max_size              = 5
+  desired_capacity      = 2
+  vpc_zone_identifier   = [
+    aws_subnet.private-eu-west-2a.id,
+    aws_subnet.private-eu-west-2b.id
+  ]
+
+  provider = aws.london
+  
+  health_check_type          = "ELB"
+  health_check_grace_period  = 300
+  force_delete               = true
+  target_group_arns          = [aws_lb_target_group.ASG01_LON_TG01.arn]
+
+  launch_template {
+    id      = aws_launch_template.app1_London_LT.id
+    version = "$Latest"
+  }
+
+  enabled_metrics = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupTotalInstances"]
+
+  # Instance protection for launching
+  initial_lifecycle_hook {
+    name                  = "instance-protection-launch"
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 60
+    notification_metadata = "{\"key\":\"value\"}"
+  }
+
+  # Instance protection for terminating
+  initial_lifecycle_hook {
+    name                  = "scale-in-protection"
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_TERMINATING"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 300
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "app1-LON-instance"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = "Production"
+    propagate_at_launch = true
+  }
+}
+
+
+# Auto Scaling Policy
+resource "aws_autoscaling_policy" "app1_LON_scaling_policy" {
+  name                   = "app1-cpu-target"
+  autoscaling_group_name = aws_autoscaling_group.ASG01_LON.name
+
+  policy_type = "TargetTrackingScaling"
+  estimated_instance_warmup = 120
+
+  provider = aws.london
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 75.0
+  }
+}
+
+# Enabling instance scale-in protection
+resource "aws_autoscaling_attachment" "ASG01_LON_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.ASG01_LON.arn
+  alb_target_group_arn   = aws_lb_target_group.ASG01_LON_TG01.arn
+  provider = aws.london
+
+}
+
+#-------------------------------------------------------------------#
+# Sao Paulo Region
+resource "aws_autoscaling_group" "ASG01_SAO" {
+  name_prefix           = "ASG01-SaoPaulo-auto-scaling-group-"
+  min_size              = 1
+  max_size              = 5
+  desired_capacity      = 2
+  vpc_zone_identifier   = [
+    aws_subnet.private-sa-east-1a.id,
+    aws_subnet.private-sa-east-1b.id
+  ]
+
+  provider = aws.saopaulo
+  
+  health_check_type          = "ELB"
+  health_check_grace_period  = 300
+  force_delete               = true
+  target_group_arns          = [aws_lb_target_group.ASG01_SAO_TG01.arn]
+
+  launch_template {
+    id      = aws_launch_template.app1_SaoPaulo_LT.id
+    version = "$Latest"
+  }
+
+  enabled_metrics = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupTotalInstances"]
+
+  # Instance protection for launching
+  initial_lifecycle_hook {
+    name                  = "instance-protection-launch"
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 60
+    notification_metadata = "{\"key\":\"value\"}"
+  }
+
+  # Instance protection for terminating
+  initial_lifecycle_hook {
+    name                  = "scale-in-protection"
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_TERMINATING"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 300
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "app1-SAO-instance"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = "Production"
+    propagate_at_launch = true
+  }
+}
+
+
+# Auto Scaling Policy
+resource "aws_autoscaling_policy" "app1_SAO_scaling_policy" {
+  name                   = "app1-cpu-target"
+  autoscaling_group_name = aws_autoscaling_group.ASG01_SAO.arn
+
+  policy_type = "TargetTrackingScaling"
+  estimated_instance_warmup = 120
+
+  provider = aws.saopaulo
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 75.0
+  }
+}
+
+# Enabling instance scale-in protection
+resource "aws_autoscaling_attachment" "ASG01_SAO_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.ASG01_SAO.arn
+  alb_target_group_arn   = aws_lb_target_group.ASG01_SAO_TG01.arn
+  provider = aws.saopaulo
+}
+
+#-------------------------------------------------------------------#
+# Australia Region
+resource "aws_autoscaling_group" "ASG01_AUS" {
+  name_prefix           = "ASG01-Australia-auto-scaling-group-"
+  min_size              = 1
+  max_size              = 5
+  desired_capacity      = 2
+  vpc_zone_identifier   = [
+    aws_subnet.private-ap-southeast-2a.id,
+    aws_subnet.private-ap-southeast-2b.id
+  ]
+
+  provider = aws.australia
+  
+  health_check_type          = "ELB"
+  health_check_grace_period  = 300
+  force_delete               = true
+  target_group_arns          = [aws_lb_target_group.ASG01_AUS_TG01.arn]
+
+  launch_template {
+    id      = aws_launch_template.app1_Australia_LT.id
+    version = "$Latest"
+  }
+
+  enabled_metrics = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupTotalInstances"]
+
+  # Instance protection for launching
+  initial_lifecycle_hook {
+    name                  = "instance-protection-launch"
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 60
+    notification_metadata = "{\"key\":\"value\"}"
+  }
+
+  # Instance protection for terminating
+  initial_lifecycle_hook {
+    name                  = "scale-in-protection"
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_TERMINATING"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 300
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "app1-AUS-instance"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = "Production"
+    propagate_at_launch = true
+  }
+}
+
+
+# Auto Scaling Policy
+resource "aws_autoscaling_policy" "app1_AUS_scaling_policy" {
+  name                   = "app1-cpu-target"
+  autoscaling_group_name = aws_autoscaling_group.ASG01_AUS.arn
+
+  policy_type = "TargetTrackingScaling"
+  estimated_instance_warmup = 120
+
+  provider = aws.australia
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 75.0
+  }
+}
+
+# Enabling instance scale-in protection
+resource "aws_autoscaling_attachment" "ASG01_AUS_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.ASG01_AUS.arn
+  alb_target_group_arn   = aws_lb_target_group.ASG01_AUS_TG01.arn
+  provider = aws.australia
+}
+
+
+#-------------------------------------------------------------------#
+# Hong Kong Region
+resource "aws_autoscaling_group" "ASG01_HK" {
+  name_prefix           = "ASG01-HongKong-auto-scaling-group-"
+  min_size              = 1
+  max_size              = 5
+  desired_capacity      = 2
+  vpc_zone_identifier   = [
+    aws_subnet.private-ap-east-1a.id,
+    aws_subnet.private-ap-east-1b.id
+  ]
+
+  provider = aws.hongkong
+  
+  health_check_type          = "ELB"
+  health_check_grace_period  = 300
+  force_delete               = true
+  target_group_arns          = [aws_lb_target_group.ASG01_HK_TG01.arn]
+
+  launch_template {
+    id      = aws_launch_template.app1_HongKong_LT.id
+    version = "$Latest"
+  }
+
+  enabled_metrics = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupTotalInstances"]
+
+  # Instance protection for launching
+  initial_lifecycle_hook {
+    name                  = "instance-protection-launch"
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 60
+    notification_metadata = "{\"key\":\"value\"}"
+  }
+
+  # Instance protection for terminating
+  initial_lifecycle_hook {
+    name                  = "scale-in-protection"
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_TERMINATING"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 300
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "app1-HK-instance"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = "Production"
+    propagate_at_launch = true
+  }
+}
+
+
+# Auto Scaling Policy
+resource "aws_autoscaling_policy" "app1_HK_scaling_policy" {
+  name                   = "app1-cpu-target"
+  autoscaling_group_name = aws_autoscaling_group.ASG01_HK.name
+
+  policy_type = "TargetTrackingScaling"
+  estimated_instance_warmup = 120
+
+  provider = aws.hongkong
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 75.0
+  }
+}
+
+# Enabling instance scale-in protection
+resource "aws_autoscaling_attachment" "ASG01_HK_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.ASG01_HK.arn
+  alb_target_group_arn   = aws_lb_target_group.ASG01_HK_TG01.arn
+  provider = aws.hongkong
+}
+
+#-------------------------------------------------------------------#
+# California Region
+resource "aws_autoscaling_group" "ASG01_CALI" {
+  name_prefix           = "ASG01-California-auto-scaling-group-"
+  min_size              = 1
+  max_size              = 5
+  desired_capacity      = 2
+  vpc_zone_identifier   = [
+    aws_subnet.private-us-west-1a.id,
+    aws_subnet.private-us-west-1b.id
+  ]
+
+  provider = aws.california
+  
+  health_check_type          = "ELB"
+  health_check_grace_period  = 300
+  force_delete               = true
+  target_group_arns          = [aws_lb_target_group.ASG01_CALI_TG01.arn]
+
+  launch_template {
+    id      = aws_launch_template.app1_California_LT.id
+    version = "$Latest"
+  }
+
+  enabled_metrics = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupTotalInstances"]
+
+  # Instance protection for launching
+  initial_lifecycle_hook {
+    name                  = "instance-protection-launch"
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 60
+    notification_metadata = "{\"key\":\"value\"}"
+  }
+
+  # Instance protection for terminating
+  initial_lifecycle_hook {
+    name                  = "scale-in-protection"
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_TERMINATING"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 300
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "app1-CALI-instance"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = "Production"
+    propagate_at_launch = true
+  }
+}
+
+
+# Auto Scaling Policy
+resource "aws_autoscaling_policy" "app1_CALI_scaling_policy" {
+  name                   = "app1-cpu-target"
+  autoscaling_group_name = aws_autoscaling_group.ASG01_CALI.arn
+
+  policy_type = "TargetTrackingScaling"
+  estimated_instance_warmup = 120
+
+  provider = aws.california
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 75.0
+  }
+}
+
+# Enabling instance scale-in protection
+resource "aws_autoscaling_attachment" "ASG01_CALI_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.ASG01_CALI.arn
+  alb_target_group_arn   = aws_lb_target_group.ASG01_CALI_TG01.arn
+  provider = aws.california
+}
